2ac44f7251f0be10e232578ce05de42927386fbb
Doan Nguyen <nguye402@gmail.com>
2017-04-19 02:09:46 -0500

    v6

---
+++ countingCharacters.js
@@ -0,0 +1,17 @@
+function countingCharacters(stringToCount){
+    // The length property has been mentioned in 
+    // the Logging Letter Exercise in Lesson 4
+    console.log (stringToCount + " has " + 
+              stringToCount.length + " characters.");
+}
+function countingCharacters2(stringToCount, characterToFind){
+
+    var count = 0;
+    var numChars = search.length; 
+    var lastIndex = str.length - numChars;
+            count++;         
+    
+    console.log("String to search in: " + str);
+    console.log("Character to find: " + search);
+    console.log("Number of times the character appears: " + count);
+}
